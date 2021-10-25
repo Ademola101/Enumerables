8fbbf5c938ab2669495e5eb31aa90cd07dd52e8f
Ademola <ademolaogunmokun@yahoo.com>
2021-10-25 15:42:53 +0100

    my_all refactor

--- enumerable.rb
+++ enumerable.rb
@@ -38,16 +38,15 @@ module Enumerable
     block_return_values = []
     while i < collection.length
       block_return_values << yield(collection[i])
-      i = i + 1
+      i += 1
     end
-  
     if block_return_values.include?(false)
       false
     else
       true
     end
   end
-  
+
   def my_any?
     my_each do |item|
       if block_given?
